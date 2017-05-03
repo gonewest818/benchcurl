9866c4b005a38f59026acc52fede199a16977769
Neil Okamoto <neil.okamoto@gmail.com>
2017-05-03 10:59:39 -0700

    parse the ab output

--- src/benchcurl/core.clj
+++ src/benchcurl/core.clj
@@ -15,19 +15,61 @@
 
 
 ;;;
-;;; Load generation
+;;; Apache Bench load generation
 ;;; 
+
+(defn ab-extract-value
+  [r v sep]
+  (let [key (keyword (-> v
+                         (clojure.string/replace " " "-")
+                         (clojure.string/lower-case)))
+        pattern (re-pattern (str "(?m)^\\s*" v sep "\\s+(\\S+)"))
+        value (second (re-find pattern r))]
+    {key value}))
+
+(defn ab-extract-percentiles
+  [r]
+  (if (re-find #"Percentage of the requests" r)
+    (into {}
+          (map #(ab-extract-value r % "")
+               ["50%" "66%" "75%" "80%" "90%" "95%" "98%" "99%" "100%"]))))
+
+(defn ab-parse-results
+  [r]
+  (let [ab-version (second (re-find #"ApacheBench, Version ([\d\.]+) " r))
+        percentiles (ab-extract-percentiles r)]
+    (into {:ab-version ab-version :percentiles percentiles}
+          (map #(ab-extract-value r % ":")
+               ["Server Software"
+                "Server Hostname"
+                "Server Port"
+                "Document Path"
+                "Document Length"
+                "Concurrency Level"
+                "Time taken for tests"
+                "Complete requests"
+                "Failed requests"
+                "Non-2xx responses"
+                "Total transferred"
+                "HTML transferred"
+                "Requests per second"
+                "Time per request"
+                "Transfer rate"]))))
 
 (defn remote-benchmark
   "Benchmark uses Apache Bench in a subprocess to generate remote load
       # ab -c <threads> -n <count> <url>/file?size=<size>
   and returns the dictionary that clojure.java.shell/sh produces"
   [server port count size threads]
-  (let [remote-url (format "http://%s:%s/file?size=%d" server port size)]
-    (sh "ab"
-        "-c" (str threads)
-        "-n" (str count)
-        remote-url)))
+  (let [remote-url (format "http://%s:%s/file?size=%d" server port size)
+        result (sh "ab"
+                   "-c" (str threads)
+                   "-n" (str count)
+                   remote-url)]
+    ;; if successful, parse output for readability in the json response
+    (if (zero? (:exit result))
+      (into result {:parsed (ab-parse-results (:out result))})
+      result)))
 
 ;;;
 ;;; Route Management
